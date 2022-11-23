9f0510a16e5218a2377981c2235f1d55929aefd6
Xurshid <kaxudi34801@gmail.com>
2022-11-23 21:31:24 +0500

    do card+hover effects+change color onclick love icon

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 'use strict';
+
+    let obj = document.getElementById("action__love");
+    obj.addEventListener('click', () => {
+        let path = document.getElementById("love");
+        path.classList.toggle('redlove');
+    })
