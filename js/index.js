cc4d23df48d65b2e49d2d5c7c6089b22c28e7411
Xurshid <kaxudi34801@gmail.com>
2022-11-24 12:01:29 +0500

    added section -prime

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
 'use strict';
-
-    let obj = document.getElementById("action__love");
-    obj.addEventListener('click', () => {
-        let path = document.getElementById("love");
-        path.classList.toggle('redlove');
-    })
